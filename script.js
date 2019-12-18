c2053b02de8303a8c831cd3ec72591102e4cc051
suba0107 <suba0107@stud.kea.dk>
2019-12-18 11:51:39 +0100

    filtrering, små rettelser og indhentning af billeder på portfolio, forside splash færdig

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+  document.addEventListener("DOMContentLoaded", start);
 
+  function start() {
+      klikFilter();
+
+  }
+
+  let filter_et;
+
+  function klikFilter() {
+      document.querySelectorAll(".filter-kollektion").forEach(elm => {
+          elm.addEventListener("click", filterKlik);
+      })
+      document.querySelectorAll(".filter-type").forEach(elm => {
+          elm.addEventListener("click", filterKlikType);
+      })
+  }
+
+
+  function filterKlikType() {
+
+      filter_et = this.dataset.type;
+      let filter_to = filter_et.toLowerCase();
+      location.href = 'smykker.html?type=' + filter_to;
+
+  }
+
+
+  function filterKlik() {
+
+      filter_et = this.dataset.collection;
+      let filter_tre = filter_et.toLowerCase();
+      location.href = 'smykker.html?kollektion=' + filter_tre;
+
+  }
