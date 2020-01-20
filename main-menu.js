072a0180b0e5a6e8c6f1a4482fbcb49cb428cb0b
suba0107 <suba0107@stud.kea.dk>
2020-01-20 12:10:20 +0100

    ændring i menuen

--- main-menu.js
+++ main-menu.js
@@ -6,13 +6,15 @@ function start() {
 }
 
 async function hentNav() {
+    /*Funktionen sættes i gang i "main-menu.js" efter DOM'en er loaded.*/
     const urlHeader = "header.html";
     const responseHeader = await fetch(urlHeader);
-    const includeHeader = await responseHeader.text();
-    document.querySelector("header").innerHTML = includeHeader;
+    const includeHeader = await responseHeader.text(); /*Når den har hentet/fetched "header.html" bruger vi text-metoden til at hente teksten ind. Den er dog hentet ind som en tekst-streng.*/
+    document.querySelector("header").innerHTML = includeHeader; /*Ved at bruge innerHTML kan vi hente det ind som HTML i stedet.*/
 
     clickBurgermenu();
 }
+
 
 
 function clickBurgermenu() {
@@ -25,17 +27,17 @@ function toggleMenu() {
     document.querySelector("#burgermenu").classList.toggle("hide");
     /*Toggle på menuen, så den åbner og lukker, når man klikker på den
      */
-    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); /*Varibel, når menuen indeholder "hide" som klasse*/
+    let erSkjult = document.querySelector("#burgermenu").classList.contains("hide"); /*Variabel, når menuen indeholder "hide" som klasse*/
 
     if (erSkjult == true) {
         document.querySelector("#menuknap").classList.remove("change");
         /*  Hvis menuen indeholder classen "skjult"
-  sæt tegnet til at være dette*/
+  sæt tegnet til at være det, som den er stylet til i css*/
     } else {
         document.querySelector("#menuknap").classList.add("change");
-        /*    Ellers skift tegn til X*/
+        /*    Ellers skift tegn til X ved at tilføje classen change*/
         document.querySelectorAll(".dropdown").forEach(elm => {
-            elm.addEventListener("click", dropDown);
+            elm.addEventListener("click", dropDown); /*for hvert dropdown-element tilføjes eventlisteneren klik*/
         })
 
     }
@@ -43,5 +45,5 @@ function toggleMenu() {
 
 
 function dropDown() {
-    document.querySelector(".dropdown-content").style.display = "block";
+    document.querySelector(".dropdown-content").style.display = "block"; /*Når der er klikket på et dropdown-element, vises indholdet*/
 }
